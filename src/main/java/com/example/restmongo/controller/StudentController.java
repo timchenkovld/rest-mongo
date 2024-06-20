f3920465d3e31651b2a5c154d9b2ad0513830b61
Тимченко Влад <timchenko.vld@gmail.com>
2024-06-20 10:41:22 +0300

    add controller

--- src/main/java/com/example/restmongo/controller/StudentController.java
+++ src/main/java/com/example/restmongo/controller/StudentController.java
@@ -1,22 +1,63 @@
 package com.example.restmongo.controller;
 
+import com.example.restmongo.exception.EntityNotFoundException;
 import com.example.restmongo.model.Student;
-import com.example.restmongo.service.StudentService;
+import com.example.restmongo.service.impl.StudentServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/students")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class StudentController {
-    private final StudentService service;
+    private final StudentServiceImpl service;
 
-    @GetMapping
+    @GetMapping("/students")
     public List<Student> fetchAllStudents() {
         return service.getAllStudents();
+    }
+
+
+    @PostMapping("/save")
+    public ResponseEntity<String> createStudent(@RequestBody Student student) {
+        try {
+            service.createStudent(student);
+            return ResponseEntity.ok("Student created successfully");
+        } catch (DuplicateKeyException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable String id) {
+        Optional<Student> studentOptional = service.findById(id);
+        return studentOptional.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateStudentById(@PathVariable String id, @RequestBody Student updatedStudent) {
+        try {
+            service.updateStudent(id, updatedStudent);
+            return ResponseEntity.ok("Student updated successfully");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteStudentById(@PathVariable String id) {
+        Optional<Student> studentOptional = service.findById(id);
+        if (studentOptional.isPresent()) {
+            service.deleteStudent(id);
+            return ResponseEntity.ok("Student deleted successfully");
+        } else {
+            throw new EntityNotFoundException(id);
+        }
     }
 }
